6ccf8d99a438265bcbe8a62cb5504b10bd4c5e76
Dina Gueye <dina3903@gmail.com>
2021-03-28 16:41:13 +0000

    Create model user

---
+++ app/Models/Personne.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Personne extends Model
+{
+    use HasFactory;
+
+    protected $fillable = array(
+        'parent_id',
+        'order',
+        'nom'
+    );
+    public static $rules = array(
+        'parent_id' => 'bigInteger',
+        'order' => 'required',
+        'nom' => 'required'
+    );
+    public function parent()
+    {
+        return $this->belongsTo(Personne::class, 'parent_id');
+    }
+}
